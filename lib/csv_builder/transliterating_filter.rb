1e0bca3e8c65466efe599812047ca33f3735908e
Alex Dilley <alex@skillstream.co.uk>
2015-09-30 16:52:00 +0100

    Don't quote `nil` values (unless `force_quotes: true`).
    Conversion of `nil` into a string causes a quoted empty string to be
    output, contradicting what the underlying CSV library would do by
    default.

--- lib/csv_builder/transliterating_filter.rb
+++ lib/csv_builder/transliterating_filter.rb
@@ -49,7 +49,7 @@ class CsvBuilder::TransliteratingFilter
     attr_accessor :output_encoding
 
     def convert_row(row)
-      row.map { |value| value.to_s.encode(output_encoding, :undef => :replace) }
+      row.map { |value| value ? value.to_s.encode(output_encoding, :undef => :replace) : value }
     end
   end
 end
